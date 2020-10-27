cfa64c2fe0729cfe540aef13c2a2146663367804
Goran <goranburhan98@gmail.com>
2020-10-28 02:00:53 +0300

    added slemani

--- index.js
+++ index.js
@@ -14,18 +14,30 @@ const water = document.querySelector(".water");
 const grass = document.querySelector(".grass");
 const choman = document.querySelector(".choman");
 
+const starynight = document.querySelector(".starrynight");
+const slemani = document.querySelector(".slemani");
+const branches = document.querySelector(".branches");
+const moon = document.querySelector(".moon");
+const slemaniText = document.querySelector(".slemaniText");
+
 window.addEventListener("scroll", () => {
   let value = window.scrollY;
   let value2 = window.scrollY - window.innerHeight;
   let value3 = window.scrollY - window.innerHeight * 2;
+  let value4 = window.scrollY - window.innerHeight * 3;
 
   sky.style.top = value * 0.5 + "px";
-  hawler.style.top = value * 0.9 + "px";
+  hawler.style.top = value * 0.8 + "px";
   mosque.style.top = value * 0.15 + "px";
   bg.style.top = value2 * 0.2 + "px";
   houses.style.top = value2 * 0.1 + "px";
   hawraman.style.top = value2 * 0.9 + "px";
-  choman.style.top = value3 * 0.9 + "px";
+  choman.style.top = value3 * 0.8 + "px";
   mountain.style.top = value3 * 0.2 + "px";
   water.style.top = value3 * 0.1 + "px";
+
+  starynight.style.top = value4 * 0.2 + "px";
+  moon.style.left = value4 * 0.15 + "px";
+  slemaniText.style.top = value4 * 0.7 + "px";
+  slemani.style.bottom = 150 + value4 * -0.2 + "px";
 });
